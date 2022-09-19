73216545ced7293afc4fe1ecf6e483292dc0fb46
Obed Barnes <obed.barnes@ucc.edu.gh>
2022-09-19 11:54:26 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
- * @n: integer pointer to a variable to be updated
- *
- * Return: nothing
- */
-
+* reset_to_98 - takes a pointer to an int as a parameter and updates the
+* value it points to to 98
+* @n: parameter to be updated
+*
+* Return: no return
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
